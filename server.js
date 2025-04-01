6bdb4f40ce49cdbe72e16500785719686a604d59
khushnam <chauhankhushnam@gmail.com>
2025-04-01 13:33:39 +0530

    cors fix

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const applicationRoutes = require("./routes/applicationRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+  }));
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
